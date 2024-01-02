0df84d258c3d13d0cb699e00caeb2e3956cb5d3b
inoulen inoulen <inoulenkon@gmail.com>
2024-01-02 23:23:47 +0530

    Add state and props to the inputField component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState<string>("");
   return (
     <div className="App">
       <span className='heading'>To Dos</span>
-      <InputField />
+      <InputField todos={todos} setTodos={setTodos}/>
     </div>
   );
-};export default App;
+};
+
+export default App;
