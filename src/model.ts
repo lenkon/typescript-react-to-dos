483005329a3c5f3f64473ea230d743b32325f2a2
inoulen inoulen <inoulenkon@gmail.com>
2024-01-21 19:27:34 +0530

    Create a TodoReducer to implement useReducer

--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,48 @@
+import { useReducer } from "react";
+
 export interface Todo {
   id: number;
   todo: string;
   isDone: boolean;
 }
+
+type Actions = 
+| { type: 'add', payload: string }
+| { type: 'remove', payload: number }
+| { type: 'done', payload: number }
+
+const TodoReducer = (state: Todo[], action: Actions) => {
+  switch (action.type) {
+    case "add":
+      return [
+        ...state,
+        { id: Date.now(), todo: action.payload, isDone: false }
+      ];
+
+    case "remove":
+      return state.filter((el) => el.id !== action.payload);
+
+    case "done":
+      return state.map((el) => 
+        el.id === action.payload ? { ...el, isDone: !el.isDone } : el
+    );
+
+    default:
+      return state;
+  }
+}
+
+const [state, dispatch] = useReducer(TodoReducer,[]);
+
+import React from 'react'
+import { Action } from "redux";
+
+const ReducerExample = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ReducerExample;
